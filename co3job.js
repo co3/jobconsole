d80aff32c056d30b210e7a9af273550d04de99eb
CO3\lhn <lhn@co3.dk>
2022-02-07 12:44:23 +0100

    new text 2

--- co3job.js
+++ co3job.js
@@ -1,7 +1,7 @@
 ( function () {
 	var boolActive = true,
 		strHeader = 'Pssst! … ',
-		strText = 'Vi hedder Co3, og vi har bygget dette website. Vi har vokseværk, og vi har mange fede projekter i pibelinen, som du har mulighed for at få ansvaret for. Tag et kig forbi vores ledige stillinger her: https://co3.dk/om-os/karriere. Udover at bygge fede løsninger, holder vi fede fester, og at sige vi er gode til bordfodbold, vil være en underdrivelse. Du kan få et kig ind i vores dagligdag her: https://co3.dk/om-os Vi glæder os til at møde dig!',
+		strText = 'Vi hedder Co3, og vi har bygget dette website. Vi har vokseværk, og vi har mange fede projekter i pibelinen, som du har mulighed for at få ansvaret for. Tag et kig forbi vores ledige stillinger her: https://co3.dk/om-os/karriere. Udover at bygge fede løsninger, holder vi fede fester, og at sige vi er gode til bordfodbold, vil være en underdrivelse. Vi glæder os til at møde dig!',
 		strLogoUrl = 'https://co3.dk/files/templates/designs/co3-2020/_assets/img/co3-logo-web-rgb.svg',
 		boolIsDarkMode = window.matchMedia && window.matchMedia( '(prefers-color-scheme: dark)' ).matches,
 		objLogo = new Image();
